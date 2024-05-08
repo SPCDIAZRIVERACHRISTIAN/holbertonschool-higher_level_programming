bde2346d6031d648c07ee405cc686b288c7d5a45
Christian <Christian>
2024-05-08 10:21:41 -0400

    Made function to print the last digit negative if the number given is negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 ld = abs(number) % 10
+if number < 0:
+    ld *= -1
 if ld > 5:
     print(f"Last digit of {number} is {ld} and is greater than 5")
 elif ld < 6 and ld != 0:
