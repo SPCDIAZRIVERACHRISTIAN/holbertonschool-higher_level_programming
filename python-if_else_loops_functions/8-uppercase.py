1b60d1d91bb62bb7ee630a08bdce224f422cce50
Christian <Christian>
2024-05-08 22:01:55 -0400

    Added file 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ''
+    for i in str:
+        if 'a' <= i <= 'z':
+            uppercase_char = chr(ord(i) - 32)
+        else:
+            uppercase_char = i
+        result += uppercase_char
+    print(result)
