93ff859e771599d37e240e4c0efbdb3a6357829c
Christian <Christian>
2024-05-10 20:03:10 -0400

    fixed pycodestyle on 2 and added 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def print_it():
     count = len(argv) - 1  # Subtract 1 to exclude the script name
-    
+
     if count == 0:
         print('0 arguments.')
     elif count == 1:
@@ -14,6 +15,6 @@ def print_it():
     for i, arg in enumerate(argv[1:], start=1):
         print('{}: {}'.format(i, arg))
 
+
 if __name__ == "__main__":
     print_it()
-
---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def add_it():
+    total = sum(int(arg) for arg in argv[1:])
+
+    print(total)
+
+if __name__ == '__main__':
+    add_it()
