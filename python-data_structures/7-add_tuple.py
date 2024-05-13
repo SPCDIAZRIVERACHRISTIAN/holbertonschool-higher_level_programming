008fee80ce64f3e7157720db4d86dacd539857d7
Christian <Christian>
2024-05-13 19:26:07 -0400

    Added file add tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,6 @@
 #!.usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    result = [0, 0]
+    a = tuple_a[:2] + (0, 0)
+    b = tuple_b[:2] + (0, 0)
 
-    for i in range(2):
-        if i < len(tuple_a) and isinstance(tuple_a[i], int):
-            result[i] += tuple_a[i]
-        if i < len(tuple_b) and isinstance(tuple_b[i], int):
-            result[i] += tuple_b[i]
-
-    return tuple(result)
+    return (a[0] + b[0], a[1] + b[1])
