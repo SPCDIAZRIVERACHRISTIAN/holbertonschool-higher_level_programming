e6b8f4c4fa9572e1e0911c1e2f9e566113eff7f4
Christian <Christian>
2024-05-17 13:48:38 -0400

    Added file 1 square

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Class named Square that defines a square."""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """initiates attribute for Square.
+
+        Args:
+            size (int): Size of the new square.
+        """
+        self.__size = size
