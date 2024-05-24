91be274a3bbb90f236d14f96d61f0b7127b3feca
Christian <Christian>
2024-05-24 14:16:40 -0400

    Added task 4 and 5

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,32 @@
+class fish:
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class bird:
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(bird, fish):
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives in both water and sky!")
+
+
+flying_fish = FlyingFish()
+flying_fish.fly()
+flying_fish.swim()
+flying_fish.habitat()
+print(flying_fish.mro())
