e0855339497c739fc438aaf00aa29ebfbf423137
Christian <Christian>
2024-05-31 15:23:11 -0400

    Added file task 04

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request, abort
+
+
+app = Flask(__name__)
+
+users = {}
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route("/data")
+def data():
+    return jsonify(list(users.keys()))
+
+
+@app.route("/status")
+def status():
+    return "OK"
+
+
+@app.route("/users/<username>")
+def users_specific(username):
+    if username not in users:
+        return jsonify({"error": "User not found"}), 404
+
+    output = users[username]
+    output["username"] = username
+
+    return jsonify(output)
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    if request.get_json() is None:
+        abort(400, "Not a JSON")
+
+    req_data = request.get_json()
+
+    if "username" not in req_data:
+        return jsonify({"error": "Username is required"}), 400
+
+    users[req_data["username"]] = {
+        "name": req_data["name"],
+        "age": req_data["age"],
+        "city": req_data["city"]
+    }
+
+    output = {
+        "username": req_data["username"],
+        "name": req_data["name"],
+        "age": req_data["age"],
+        "city": req_data["city"]
+    }
+    return jsonify({"message": "User added", "user": output}), 201
+
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
