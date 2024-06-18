42769e50f9de47a1bdf2749bd289594517908880
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-06-17 20:19:02 -0400

    Refactor SQL query to select score and name from second_table

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- This is a script that lists all records of the table second_table
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
