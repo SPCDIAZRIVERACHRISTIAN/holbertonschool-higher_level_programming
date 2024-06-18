9ca3405534e84771de81ff136a7a5ed17be6c469
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-06-18 10:26:13 -0400

    chore: Add FLUSH PRIVILEGES statement to create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- This script creates a user with all privileges even if it exists grants all privileges on/
--- *.* to 'user_0d_1' @ 'localhost' identified by 'user_0d_1_pwd' with grant option;
+-- This script creates a user with all privileges even if it exists grants all privileges on all databases and flushes privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
