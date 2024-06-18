e09c45583b3dbd504c475168ea4972eeeda73144
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-06-18 10:32:16 -0400

    chore: Update create_user.sql to grant privileges to 'user_0d_1' from 'host'

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- This script creates a user with all privileges even if it exists grants all privileges on all databases and flushes privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'host' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
