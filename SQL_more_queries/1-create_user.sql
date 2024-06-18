6918383f392b372bb8b8fcf7e7e851b348de90cd
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-06-18 10:22:55 -0400

    chore: Add FLUSH PRIVILEGES statement to create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,3 +2,4 @@
 -- *.* to 'user_0d_1' @ 'localhost' identified by 'user_0d_1_pwd' with grant option;
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
