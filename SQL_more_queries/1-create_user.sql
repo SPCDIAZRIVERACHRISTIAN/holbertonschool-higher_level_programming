f005916fc07ca03ee0f401049f7dba0d8879fbe5
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-06-18 10:39:54 -0400

    Refactor create_user.sql to use localhost instead of '%' for user_0d_1 privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- This script creates a user with all privileges even if it exists grants all privileges on all databases and flushes privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
