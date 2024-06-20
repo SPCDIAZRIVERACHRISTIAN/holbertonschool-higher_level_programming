03c6a42e8e3628c851a113cd0e3adf79c22d923b
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-06-20 14:38:13 -0400

    Added file 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
-        if "N" in row[1]:
+        if 'N' in row[1]:
             print(row)
     cur.close()
     db.close()
