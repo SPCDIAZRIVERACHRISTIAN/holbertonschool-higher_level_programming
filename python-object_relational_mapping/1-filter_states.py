3f57c679b85e2f762e03c0f397a239753297c429
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-06-20 14:25:30 -0400

    Added file 1 for tasks

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-'''This script selects all staTES WITH A NAME THAT STARTS WITH THE LETTER N'''
-import MySQLdb #type: ignore
+'''This script selects all state that start with letter N'''
+import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         password=sys.argv[2], db=sys.argv[3], charset='utf8')
+                         password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
